128bbaa92e739cdb18be3cc2cfd5ab4374731051
Erica Pantoja <emilianapantoja90@gmail.com>
2019-03-02 21:25:02 -0600

    Add files via upload

---
+++ SurgeonClass.js
@@ -0,0 +1,16 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name(){
+    return this._name;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+console.log(surgeonCurry);
+console.log(surgeonDurant);
